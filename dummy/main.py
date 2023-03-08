f8148954aae03a771480a38e5cc06ae7289fcd43
pulical <51207498+pulical@users.noreply.github.com>
2023-03-08 10:21:13 +0530

    ssss

--- dummy/main.py
+++ dummy/main.py
@@ -1,16 +1,18 @@
 import pytest
 
 def test_addition():
-    assert 2+2 == 4
+    assert 2 + 2 == 4
 
 def test_subtraction():
-    assert 5-2 == 3
+    assert 5 - 3 == 2
 
 def test_multiplication():
-    assert 3*4 == 12
+    assert 3 * 4 == 12
 
 def test_division():
-    assert 8/2 == 4
+    assert 8 / 4 == 2
 
-if __name__ == '__main__':
-    pytest.main(['-v', '-s', '--html=report.html'])
+# You can add more test functions here
+
+if __name__ == "__main__":
+    pytest.main(['-v', '--html=report.html'])
